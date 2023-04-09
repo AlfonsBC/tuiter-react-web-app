8f9a3589d14baed0c8c1e2f3122808bdcfa6cd2e
AlfonsBC <albace2000@hotmail.com>
2023-04-09 15:37:26 -0400

    a9 spring23

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,15 +1,12 @@
 import React from "react";
-//import PostList from "../post-list";
 import TuitsList from "../tuits/tuit-list"
 import WhatsHappening from "./whats-happening";
 
 const HomeComponent = () => {
     return(
         <>
-            
             <WhatsHappening/>
             <TuitsList/>
-
         </>
     );
 };
